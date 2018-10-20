82497b99b417aee1f58ff07a99adf0a614477af6
Ti-Tom <thomas@laella.net>
2018-10-20 11:57:02 +0200

    Best practice: add curly braces

--- Pragma/Forms/Fields/AbstractCheckableField.php
+++ Pragma/Forms/Fields/AbstractCheckableField.php
@@ -28,17 +28,22 @@ class AbstractCheckableField implements FieldsInterface{
 		$object = isset($this->form) && !empty($this->form->object) ? $this->form->object : null;
 
 		$field =  '<input type="'.$this->type.'" name="'.$this->name.'" id="'.$this->id.'" value="'.$this->value.'" ';
-		if( ! is_null($this->classes) && ! empty($this->classes) ) $field .= ' class="'.$this->classes.'" ';
+		if( ! is_null($this->classes) && ! empty($this->classes) ){
+			$field .= ' class="'.$this->classes.'" ';
+		}
 
 		$column = $this->db_field;
 		if( ! is_null($this->db_field) && ! empty($this->db_field) && ! is_null($object) && isset($object->$column)	&& $object->$column == $this->value){
 			$field .= ' checked="checked" ';
 		}
-		else if( $this->checked ) $field .= ' checked="checked" ';
-
+		elseif( $this->checked ){
+			$field .= ' checked="checked" ';
+		}
 
 		$field .= ' '.$this->additional_attributes. ' />';
-		if( ! is_null($this->dom_extension) ) $field .= $this->dom_extension;
+		if( ! is_null($this->dom_extension) ){
+			$field .= $this->dom_extension;
+		}
 
 		return $field;
 	}
